c895145972418999f244a869090fb6a4ec442ca3
Fl0w111 <33512923+Fl0w111@users.noreply.github.com>
2017-11-14 10:49:34 +0100

    Update google_map.js

--- js/google_map.js
+++ js/google_map.js
@@ -5,7 +5,7 @@ function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(49.82725819999999, 10.17522770000005);
+    var myLatlng = new google.maps.LatLng(lat: 49.82725819999999, lng: 10.17522770000005);
     // 39.399872
     // -8.224454
     
